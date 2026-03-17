5aa007dc64dc0b5a8bb53f22f01228d924976fd7
Anna Bilytskykh <anna2white@gmail.com>
2026-03-17 21:51:58 +0200

    Доашка виконанан, пункти 1-4

---
+++ jsBasics/task4.js
@@ -0,0 +1,16 @@
+// 4.1 Площа кола
+let radiusCircle = 7;
+let circleArea = Math.PI * Math.pow(radiusCircle, 2);
+console.log("Площа кола:", circleArea.toFixed(2));
+
+// 4.2 Площа прямокутника
+let length = 10;
+let width = 5;
+let rectangleArea = length * width;
+console.log("Площа прямокутника:", rectangleArea.toFixed(2));
+
+// 4.3 Об'єм циліндра
+let radiusCylinder = 3;
+let heightCylinder = 7;
+let cylinderVolume = Math.PI * Math.pow(radiusCylinder, 2) * heightCylinder;
+console.log("Об'єм циліндра:", cylinderVolume.toFixed(2));
